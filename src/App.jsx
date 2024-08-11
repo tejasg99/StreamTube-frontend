22d6146839bea421b2d6adb0f518b694eb2fad74
Tejas Gawade <tgawade092@gmail.com>
2024-08-11 12:20:18 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-white'>
+      <h1 className='text-center font-bold text-black'>VideoTube frontend project</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
