2cffcd99c504f8da21b2d7b495cd3a06c793c649
Tejas Gawade <tgawade092@gmail.com>
2024-08-18 12:05:58 +0530

    Add userAPI

---
+++ src/api/userAPI.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { API_URL } from "../constants";
+import toast from "react-hot-toast";
+
+// Axios instance
+const instance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true
+})
+
+export const getUserChannelProfile = async (username) => {
+    try {
+        const { data } = await instance.get(`/users/c/${username}`)
+        return data?.data
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
+
+export const getWatchHistory = async () => {
+    try {
+        const { data } = await instance.get("/users/watch-history")
+        return data?.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
+
+export const updateAccountDetails = async (updateData) => {
+    try {
+        const { data } = await instance.patch("/users/update-account-details", updateData)
+        toast.success(data?.message);
+        return data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
+
+export const updateAvatar = async (avatarData) => {
+    const formData = new formData();
+
+    if(avatarData){
+        formData.append("avatar", avatarData)
+    }
+
+    try {
+        const { data } = await instance.patch("/users/avatar", formData)
+        toast.success(data?.message);
+        return data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
+
+export const updateCoverImage = async (coverImageData) => {
+    const formData = new formData()
+
+    if(coverImageData){
+        formData.append("coverImage", coverImageData)
+    }
+
+    try {
+        const { data } = await instance.patch("/users/cover-image", formData)
+        toast.success(data?.message)
+        return data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
+
+
+
