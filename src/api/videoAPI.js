5b895ecf07d95436c6d1ecf83acb301f4d306d80
Tejas Gawade <tgawade092@gmail.com>
2024-08-19 19:16:00 +0530

    Add videoAPI

---
+++ src/api/videoAPI.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { API_URL } from "../constants";
+import toast from "react-hot-toast";
+
+// Axios instance
+const instance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true
+})
+
+export const publishVideo = async (videoData) => {
+    const formData = new formData();
+    
+    formData.append("videoFile", videoData.videoFile)
+    formData.append("title", videoData.title)
+    formData.append("thumbnail", videoData.thumbnail)
+    formData.append("description", videoData.description)
+    formData.append("owner", videoData.owner)
+    formData.append("duration", videoData.duration)
+
+    try {
+        const { data } = await instance.post("/videos", formData);
+        toast.success(data?.message);
+        return data?.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
