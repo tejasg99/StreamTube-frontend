12bb919fe4093a431ffdfc7d7182aa3f323bd699
Tejas Gawade <tgawade092@gmail.com>
2024-08-25 14:07:09 +0530

    Add subscriptionAPI

---
+++ src/api/subscriptionAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_URL } from "../constants";
+import toast from "react-hot-toast";
+
+// Axios instance
+const instance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true
+})
+
+export const toggleSubscription = async (channelId) => {
+    try {
+        const { data } = await instance.post(`/subscriptions/c/${channelId}`)
+        toast.success(data?.message)
+        return data?.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error?.response?.data?.error;
+    }
+}
