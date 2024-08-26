5bc2c274f64bfe99bc273c510eaeffbf3ae0da75
Tejas Gawade <tgawade092@gmail.com>
2024-08-26 11:57:25 +0530

    Add getChannelStats method in dashboardAPI

--- src/api/dashboardAPI.js
+++ src/api/dashboardAPI.js
@@ -7,3 +7,14 @@ const instance = axios.create({
     baseURL: API_URL,
     withCredentials: true,
 })
+
+export const getChannelStats = async (channelId) => {
+    try {
+        const { data } = await instance.get(`/dashboard/stats/${channelId}`)
+        toast.success(data?.message)
+        return data?.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error)
+        throw error?.response?.data?.error;
+    }
+}
