3d91bd67a54ea2d72d1e138a9c760866a2d10b7f
Tejas Gawade <tgawade092@gmail.com>
2024-08-27 08:46:42 +0530

    react-hot-toast configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { Provider } from 'react-redux'
-import store from './store/store.js'
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { Toaster } from "react-hot-toast";
 
-import Home from './pages/Home.jsx'
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,13 +17,26 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-      }
-    ]
-  }
-])
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </Provider>,
-)
+  <Toaster
+    position="top-right"
+    reverseOrder={true}
+    toastOptions={{
+      error: {
+        style: { borderRadius: "0", color: "red" },
+      },
+      success: {
+        style: { borderRadius: "0", color: "green" },
+      },
+      duration: 2000,
+    }}
+  />
+);
