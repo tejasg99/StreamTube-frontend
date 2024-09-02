50434e5f6dc710c95e82990bed5acf03964356c7
Tejas Gawade <tgawade092@gmail.com>
2024-09-02 11:39:52 +0530

    Complete playlist hook

--- src/hooks/playlist.hook.js
+++ src/hooks/playlist.hook.js
@@ -38,3 +38,39 @@ export const useAddVideoToPlaylist = () => {
         }
     })
 }
+
+export const useRemoveVideoFromPlaylist = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({videoId, playlistId}) => removeVideoFromPlaylist(videoId, playlistId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["playlists"]})
+        }
+    })
+}
+
+export const useGetPlaylistById = (playlistId) => {
+    return useQuery({
+        queryKey: ["playlist", playlistId],
+        queryFn: () => getPlaylistById(playlistId),
+        staleTime: 1000*60*4,
+    })
+}
+
+export const useGetUserPlaylists = (userId) => {
+    return useQuery({
+        queryKey: ["playlists", userId],
+        queryFn: () => getUserPlaylists(userId),
+        staleTime: 1000*60*4,
+    })
+}
+
+export const useDeletePlaylist = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (playlistId) => deletePlaylist(playlistId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["playlists"]})
+        }
+    })
+}
