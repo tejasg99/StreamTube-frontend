c692e13836a85437d4087f616f364bcdfebe4d5a
Tejas Gawade <tgawade092@gmail.com>
2024-09-05 19:39:29 +0530

    Add Button component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header/Header.jsx";
 import Search from "./Header/Search.jsx";
 import Sidebar from "./Sidebar.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Button from "./Button.jsx"
 
 export {
     Header,
     Search,
     Sidebar,
     Footer,
+    Button
 }
