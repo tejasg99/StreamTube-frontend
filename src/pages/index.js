81f35246a126256e0c550fdf426fb1218969492b
Tejas Gawade <tgawade092@gmail.com>
2024-09-23 17:07:33 +0530

    Complete all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,45 @@
-import Home from "./Home"
-
-
-
+import Home from "./Home";
+import EditProfile from "./EditProfile";
+import History from "./History";
+import LikedVideos from "./LikedVideos";
+import Login from "./Login";
+import MyChannel from "./MyChannel";
+import MyDashboard from "./MyDashboard";
+import Playlist from "./Playlist";
+import SearchVideos from "./SearchVideos";
+import Signup from "./Signup";
+import Subscriptions from "./Subscriptions";
+import Support from "./Support";
+import TweetPage from "./TweetPage";
+import VideoDetail from "./VideoDetail";
+import ChannelAbout from "./Channel/ChannelAbout";
+import ChannelPlaylist from "./Channel/ChannelPlaylist";
+import ChannelSubscribers from "./Channel/ChannelSubscribers";
+import ChannelTweets from "./Channel/ChannelTweets";
+import ChannelVideos from "./Channel/ChannelVideos";
+import EditAccountDetails from "./EditProfile/EditAccountDetails";
+import EditChangePassword from "./EditProfile/EditChangePassword";
 
 export default{
-    Home
+    Home,
+    EditProfile,
+    History,
+    LikedVideos,
+    Login,
+    Signup,
+    MyChannel,
+    MyDashboard,
+    Playlist,
+    SearchVideos,
+    Subscriptions,
+    Support,
+    TweetPage,
+    VideoDetail,
+    ChannelAbout,
+    ChannelPlaylist,
+    ChannelSubscribers,
+    ChannelTweets,
+    ChannelVideos,
+    EditAccountDetails,
+    EditChangePassword,
 }
