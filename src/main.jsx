6b0ebe1ed00e4bf6426250662491d088c8630412
Tejas Gawade <tgawade092@gmail.com>
2024-09-27 23:54:56 +0530

    Fixes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,47 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy, Suspense } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AuthLayout from "./components/AuthLayout.jsx";
+import { AuthLayout } from "./components";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import store from "./store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-if (import.meta.env.MODE === "development") disableReactDevTools();
+
+if (import.meta.env.MODE === "development") {
+  disableReactDevTools();
+}
 
 const App = lazy(() => import("./App.jsx"));
-const Home = lazy(() => import("./pages/Home.jsx"));
 const Signup = lazy(() => import("./pages/Signup.jsx"));
 const Login = lazy(() => import("./pages/Login.jsx"));
+const Home = lazy(() => import("./pages/Home.jsx"));
 const VideoDetail = lazy(() => import("./pages/VideoDetail.jsx"));
 const LikedVideos = lazy(() => import("./pages/LikedVideos.jsx"));
 const MyChannel = lazy(() => import("./pages/MyChannel.jsx"));
 const MyDashboard = lazy(() => import("./pages/MyDashboard.jsx"));
 const Subscriptions = lazy(() => import("./pages/Subscriptions.jsx"));
+const Support = lazy(() => import("./pages/Support.jsx"));
 const History = lazy(() => import("./pages/History.jsx"));
 const ChannelPlaylist = lazy(() =>
   import("./pages/Channel/ChannelPlaylist.jsx")
 );
+const ChannelAbout = lazy(() => import("./pages/Channel/ChannelAbout.jsx"));
 const ChannelSubscribers = lazy(() =>
   import("./pages/Channel/ChannelSubscribers.jsx")
 );
-const ChannelVideos = lazy(() => import("./pages/Channel/ChannelVideos.jsx"));
 const ChannelTweets = lazy(() => import("./pages/Channel/ChannelTweets.jsx"));
-const ChannelAbout = lazy(() => import("./pages/Channel/ChannelAbout.jsx"));
-const EditChangePassword = lazy(() =>
-  import("./pages/EditProfile/EditChangePassword.jsx")
-);
+const ChannelVideos = lazy(() => import("./pages/Channel/ChannelVideos.jsx"));
+const EditProfile = lazy(() => import("./pages/EditProfile.jsx"));
 const EditAccountDetails = lazy(() =>
   import("./pages/EditProfile/EditAccountDetails.jsx")
 );
-const EditProfile = lazy(() => import("./pages/EditProfile.jsx"));
-const Support = lazy(() => import("./pages/Support.jsx"));
+const EditChangePassword = lazy(() =>
+  import("./pages/EditProfile/EditChangePassword.jsx")
+);
 const Playlist = lazy(() => import("./pages/Playlist.jsx"));
 const SearchVideos = lazy(() => import("./pages/SearchVideos.jsx"));
 const TweetPage = lazy(() => import("./pages/TweetPage.jsx"));
@@ -49,16 +52,30 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-        <App />
-      </Suspense>
+      <AuthLayout auth={false}>
+        <Suspense
+          fallback={
+            <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+              Loading...
+            </p>
+          }
+        >
+          <App />
+        </Suspense>
+      </AuthLayout>
     ),
     children: [
       {
         path: "/",
         element: (
           <AuthLayout auth={false}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
+            <Suspense
+              fallback={
+                <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                  Loading...
+                </p>
+              }
+            >
               <Home />
             </Suspense>
           </AuthLayout>
@@ -68,7 +85,13 @@ const router = createBrowserRouter([
         path: "/videos/:videoId",
         element: (
           <AuthLayout auth={false}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
+            <Suspense
+              fallback={
+                <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                  Loading...
+                </p>
+              }
+            >
               <VideoDetail />
             </Suspense>
           </AuthLayout>
@@ -78,201 +101,315 @@ const router = createBrowserRouter([
         path: "/liked-videos",
         element: (
           <AuthLayout auth={true} pageName={"LikedVideos"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <LikedVideos />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <LikedVideos />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
         path: "/watch-history",
         element: (
-          <AuthLayout auth={true} pageName={"History"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <History />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={true} pageName={"watchHistory"}> 
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <History />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
-        path: "/channel/:username",
+        path: "/channel/:username/",
         element: (
-          <AuthLayout auth pageName={"MyChannel"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <MyChannel />
-            </Suspense>
-          </AuthLayout>
+          <AuthLayout auth={true} pageName={"MyChannel"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <MyChannel />
+          </Suspense>
+        </AuthLayout>
         ),
         children: [
           {
-            path: "videos",
+            path: "/channel/:username/videos",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <ChannelVideos />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"ChannelVideos"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <ChannelVideos />
+              </Suspense>
+            </AuthLayout> 
+            )
           },
           {
-            path: "tweets",
+            path: "/channel/:username/tweets",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <ChannelTweets />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"ChannelTweets"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <ChannelTweets />
+              </Suspense>
+            </AuthLayout>
+            )
           },
           {
-            path: "playlist",
+            path: "/channel/:username/playlist",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <ChannelPlaylist />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"ChannelPlaylists"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <ChannelPlaylist />
+              </Suspense>
+            </AuthLayout>
+            )
           },
           {
-            path: "subscribers",
+            path: "/channel/:username/subscribers",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <ChannelSubscribers />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"ChannelSubscribers"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <ChannelSubscribers />
+              </Suspense>
+            </AuthLayout>
+            )
           },
           {
-            path: "about",
+            path: "/channel/:username/about",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <ChannelAbout />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"ChannelAbout"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <ChannelAbout />
+              </Suspense>
+            </AuthLayout>
+            )
           },
-        ],
+        ]
       },
       {
         path: "/myDashboard",
         element: (
-          <AuthLayout auth pageName={"MyStudio"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <MyDashboard />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={true} pageName={"MyDashboard"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <MyDashboard />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
         path: "/subscriptions",
         element: (
-          <AuthLayout auth pageName={"Subscriptions"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <Subscriptions />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={true} pageName={"Subscriptions"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <Subscriptions />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
         path: "/edit-profile",
         element: (
-          <AuthLayout auth pageName={"Settings"}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <EditProfile />
-            </Suspense>
-          </AuthLayout>
+          <AuthLayout auth={true} pageName={"EditProfile"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <EditProfile />
+          </Suspense>
+        </AuthLayout>
         ),
         children: [
           {
-            path: "change-password",
+            path: "/edit-profile/change-password",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <EditChangePassword />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"EditChangePassword"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <EditChangePassword />
+              </Suspense>
+            </AuthLayout>
+            )
           },
           {
-            path: "account-details",
+            path: "/edit-profile/edit-account-details",
             element: (
-              <AuthLayout auth>
-                <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-                  <EditAccountDetails />
-                </Suspense>
-              </AuthLayout>
-            ),
+              <AuthLayout auth={true} pageName={"EditAccountDetails"}>
+              <Suspense
+                fallback={
+                  <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                    Loading...
+                  </p>
+                }
+              >
+                <EditAccountDetails />
+              </Suspense>
+            </AuthLayout>
+            )
           },
-        ],
+        ]
       },
       {
         path: "/playlist/:playlistId",
         element: (
-          <AuthLayout auth>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <Playlist />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={true} pageName={"Playlist"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <Playlist />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
         path: "/support",
         element: (
-          <AuthLayout auth={false}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <Support />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={false} pageName={"Support"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <Support />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
         path: "/search/:query",
         element: (
-          <AuthLayout auth={false}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <SearchVideos />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={false} pageName={"SearchVideos"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <SearchVideos />
+          </Suspense>
+        </AuthLayout>
+        )
       },
       {
-        path: "/tweets/",
+        path: "/tweets-page",
         element: (
-          <AuthLayout auth={false}>
-            <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-              <TweetPage />
-            </Suspense>
-          </AuthLayout>
-        ),
+          <AuthLayout auth={false} pageName={"TweetPage"}>
+          <Suspense
+            fallback={
+              <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+                Loading...
+              </p>
+            }
+          >
+            <TweetPage />
+          </Suspense>
+        </AuthLayout>
+        )
       },
     ],
   },
   {
-    path: "/login",
-    element: (
-      <AuthLayout auth={false}>
-        <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-          <Login />
-        </Suspense>
-      </AuthLayout>
-    ),
-  },
-  {
     path: "/signup",
     element: (
-      <AuthLayout auth={false}>
-        <Suspense fallback={<p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">Loading...</p>}>
-          <Signup />
-        </Suspense>
-      </AuthLayout>
-    ),
+      <AuthLayout auth={false} pageName={"Signup"}>
+      <Suspense
+        fallback={
+          <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+            Loading...
+          </p>
+        }
+      >
+        <Signup />
+      </Suspense>
+    </AuthLayout>
+    )
   },
+  {
+    path: "/login",
+    element: (
+      <AuthLayout auth={false} pageName={"Login"}>
+      <Suspense
+        fallback={
+          <p className="text-slate-100 text-3xl text-center w-full h-screen bg-slate-900">
+            Loading...
+          </p>
+        }
+      >
+        <Login />
+      </Suspense>
+    </AuthLayout>
+    )
+  }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
