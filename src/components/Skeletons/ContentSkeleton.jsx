f709cc30256d938e3edb3a3da1bee645af82401e
Tejas Gawade <tgawade092@gmail.com>
2024-10-31 16:03:20 +0530

    Fix skeletons

--- src/components/Skeletons/ContentSkeleton.jsx
+++ src/components/Skeletons/ContentSkeleton.jsx
@@ -1,19 +1,19 @@
 function ContentSkeleton() {
   return (
     <div className="flex-grow p-4">
-        <div className="w-full h-auto flex gap-3 my-2 bg-transparent">
+        <div className="w-full h-auto flex flex-wrap gap-3 my-2 bg-transparent">
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
         </div>
-        <div className="w-full h-auto flex gap-3 my-2">
+        <div className="w-full h-auto hidden md:flex gap-3 my-2">
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
         </div>
-        <div className="w-full h-auto flex gap-3 my-2">
+        <div className="w-full h-auto hidden md:flex gap-3 my-2">
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
             <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
--- src/components/Skeletons/HeaderSkeleton.jsx
+++ src/components/Skeletons/HeaderSkeleton.jsx
@@ -2,8 +2,8 @@ function SkeletonHeader() {
   return (
     <div className="h-16 bg-transparent animate-pulse flex justify-between" >
       <div className="h-full w-1/4 bg-slate-800 rounded-lg"></div>
-      <div className="h-full w-1/3 bg-slate-800 rounded-lg"></div>
-      <div className="h-full w-1/3 bg-slate-800 rounded-lg"></div>
+      <div className="h-full w-1/3 hidden md:block bg-slate-800 rounded-lg"></div>
+      <div className="h-full w-1/3 hidden md:block bg-slate-800 rounded-lg"></div>
     </div>
   );
 }
