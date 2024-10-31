acef61f83dedb17af0e7a6263204e5ac345f8ecb
Tejas Gawade <tgawade092@gmail.com>
2024-10-31 15:39:56 +0530

    Adjust sidebar skeleton responsiveness

--- src/components/Skeletons/SidebarSkeleton.jsx
+++ src/components/Skeletons/SidebarSkeleton.jsx
@@ -1,6 +1,6 @@
 function SkeletonSidebar() {
   return (
-    <div className="w-64 h-screen bg-transparent animate-pulse flex flex-col gap-20">
+    <div className="w-64 h-screen bg-transparent animate-pulse flex flex-col gap-20 sm:hidden md:block">
       <div className="top">
         <div className="h-12 w-[95%] bg-slate-800 my-2 rounded-lg mx-1"></div>
         <div className="h-12 w-[95%] bg-slate-800 my-2 rounded-lg mx-1"></div>
