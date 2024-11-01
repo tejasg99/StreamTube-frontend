a106fb786b0a2fa334819599b5b1388711275d42
Tejas Gawade <tgawade092@gmail.com>
2024-11-01 16:29:57 +0530

    Fix content skeleton responsiveness

--- src/components/Skeletons/ContentSkeleton.jsx
+++ src/components/Skeletons/ContentSkeleton.jsx
@@ -2,10 +2,10 @@ function ContentSkeleton() {
   return (
     <div className="flex-grow p-4">
       <div className="w-full h-auto flex flex-wrap md:flex-nowrap gap-3 my-2 bg-transparent">
-        <div className="h-[200px] w-[400px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
-        <div className="h-[200px] w-[400px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
-        <div className="h-[200px] w-[400px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
-        <div className="h-[200px] w-[400px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
+        <div className="h-[200px] w-[300px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
+        <div className="h-[200px] w-[300px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
+        <div className="h-[200px] w-[300px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
+        <div className="h-[200px] w-[300px] md:w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
       </div>
       <div className="w-full h-auto hidden md:flex gap-3 my-2">
         <div className="h-[200px] w-1/4 bg-slate-800 animate-pulse rounded-lg"></div>
